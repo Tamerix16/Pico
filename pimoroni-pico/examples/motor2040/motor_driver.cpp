c195d2798c82963700a18e7f841736f92c19fcfb
Tamerix16 <darcy.stewart2005@outlook.com>
2025-09-25 14:38:08 +1000

    add serial string

--- pimoroni-pico/examples/motor2040/motor_driver.cpp
+++ pimoroni-pico/examples/motor2040/motor_driver.cpp
@@ -1,33 +1,66 @@
 
 #include <cstdio>
 #include "pico/stdlib.h"
+#include "motor2040.hpp"
+using namespace motor;
+ //initialise communication
+Motor m = Motor(motor2040::MOTOR_A);
+volatile int position = 0;
+volatile bool READY = false;
 
-    volatile char test[5];
+void drive_forward()
+{
+    m.full_positive();
+}
+void soft_stop()
+{
+    m.coast();
+}
+int main() 
+{
+    stdio_init_all();
+    // Your other initialization code
+    m.init(); //initialise motor
+    m.enable();
     int position = 0;
-
-    int main() 
+    char str[100];
+    while (1) 
     {
-        stdio_init_all();
-        // Your other initialization code
-        while (1) 
+        int c = getchar_timeout_us(0); // Check for a character immediately
+        
+        if (c!=PICO_ERROR_TIMEOUT)
         {
-            int c = getchar_timeout_us(0); // Check for a character immediately
-            if (c != PICO_ERROR_TIMEOUT) 
-            {
-            // Process the received character 'c'
-            if (c == '\n')
-                {
-                    char backup[]="test";
-                printf("Received: %s\n", backup);
-                position = 0;
-                }
-                else
-                {
-                    test[position] = c;
-                    position ++;
-                    printf("Received: %c\n", (char)c);
-                }
-            }
+            position = 0;
+        do
+        {
+            str[position] = (char)c;
+            position++;
+            READY = true;
+        }while((c = getchar_timeout_us(0))!='\n'&& c!= PICO_ERROR_TIMEOUT);
+        str[position] = '\0';
+        printf ("string %c",str[3]);
         }
-        return 0;
+           
+    
+        // Process the received character 'c'
+        /*
+        if (char(c)=='m')
+        {
+        drive_forward();
+        }
+        else if(char(c)=='s')
+        {
+            soft_stop();
+        }
+        else if(c==100)
+        {
+        printf("hi");
+        }
+        printf("%c\n", char(c));
+        */
+    
+        
     }
+    return 0;
+}
+
